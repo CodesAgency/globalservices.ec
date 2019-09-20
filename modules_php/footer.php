92628df1ed7b5fe5adf7ea0738e6c39baefb5a8b
akali <andrenimacion@gmail.com>
2019-09-20 15:00:23 -0500

    se agregaron los productos

--- modules_php/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="container-fluid" id="footer">
-    <div id="f1">
-        <figure>
-            <img src="./assets/img/GLOBAL-SERVICES-LOGO-W.png" width="200px" height="auto">
-        </figure>
-    </div>
-</div>
